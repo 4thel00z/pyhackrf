434bde2eb61540d121a240bc9fece77944d89725
4thel00z <4thel00z@gmail.com>
2021-06-04 01:14:16 +0200

    [master] Bump version to 0.1.4
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- hackrf/__init__.py
+++ hackrf/__init__.py
@@ -1,7 +1,7 @@
 # TODO: only use transfer->valid_length in callbacks
 # TODO: make error messages more informative
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from ctypes import *
 import logging
