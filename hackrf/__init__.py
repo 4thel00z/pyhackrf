6cd2ea3d0305dd9d663e648ce5a7dd8a78ff7971
4thel00z <4thel00z@gmail.com>
2021-06-04 00:02:23 +0200

    [master] Bump version
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- hackrf/__init__.py
+++ hackrf/__init__.py
@@ -1,7 +1,7 @@
 # TODO: only use transfer->valid_length in callbacks
 # TODO: make error messages more informative
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from ctypes import *
 import logging
